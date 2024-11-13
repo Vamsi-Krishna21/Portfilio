83201b980b49a9b3e9c53a74043276ef12ca3240
Vamsi-Krishna21 <vamsikumbhala@gmail.com>
2024-11-14 00:05:03 +0530

    "Cleaar description of projects"

--- portfolio/src/app/components/experience/experience.component.ts
+++ portfolio/src/app/components/experience/experience.component.ts
@@ -15,14 +15,19 @@ export class ExperienceComponent {
       company: 'Infosys Limited',
       dates: 'Nov 2021 – Present',
       responsibilities: [
-        'Developed and maintained web applications using Angular and JavaScript.',
-        'Integrated RESTful APIs and third-party services.',
-        'Conducted comprehensive software testing including unit, integration, and end-to-end.',
-        'Collaborated with cross-functional teams.',
-        'Optimized application performance through code reviews and refactoring.',
-        'Implemented responsive design for compatibility across devices.',
-        'Participated in Agile and Scrum processes.'
-      ],
+        'Developed and maintained web applications using the Angular framework and JavaScript for front-end development.Utilized Jasmine and Karma for writing unit tests and improving code quality.',
+        'Integrated RESTful APIs and third-party services to expand application features and improve user engagement,enhancing functionality and interactivity',
+        'Conducted comprehensive software testing, including unit, integration, and end-to-end testing, to identify and resolve software issues, ensuring delivery of high-quality, bug-free applications.',
+        'Collaborated cross-functionally with product, design, to gather requirements, design and implement solutions, and alignfeatures with business objectives.',
+        'Optimized application performance by performing code reviews, refactoring code, and implementing best practices for faster load times and improved application responsiveness.',
+        'Applied responsive web design principles to ensure compatibility across mobile devices, tablets, and desktops, improving accessibility and user experience.',
+        'Provided maintenance and updates for existing applications, delivering support and enhancements based on user feedback and evolving project requirements.',
+        'Actively participated in Agile and Scrum processes, including daily stand-ups, sprint planning, and retrospectives, to improve team collaboration, project planning, and delivery timelines.',
+        'Authored comprehensive documentation for codebases, API specifications, and application workflows, streamlining onboarding and supporting knowledge transfer.',
+        'Developed and executed unit tests and integration tests with testing frameworks like Jasmine and Karma, improving software reliability, scalability, and maintainability.',
+        'Mentored junior developers by providing technical guidance, sharing best practices, and assisting with troubleshootingto foster professional growth.',
+        'Supported testing team by doing Manual and regression testing along with developement for 3 months.',
+        'Engaged in continuous skill development through courses, certifications, and professional development activities to stay current with emerging technologies and industry standards'],
       expanded: false
     }
   ];
--- portfolio/src/app/components/projects/projects.component.ts
+++ portfolio/src/app/components/projects/projects.component.ts
@@ -14,29 +14,36 @@ export class ProjectsComponent {
       name: 'Focus 2.0',
       techStack: 'Angular, SCSS, HTML5, Angular Material, Bootstrap',
       description: [
-        'Collaborated with Business Analysts to gather requirements.',
-        'Engineered responsive user interfaces.',
-        'Integrated GraphQL APIs.',
-        'Recommended and implemented PrimeNG and NG-Toast libraries.'
+        'Collaborated with Business Analysts to gather and analyze requirements for applications related to development of new screens and implementing new functionalities.',
+        'Engineered responsive and dynamic user interfaces using Angular enhancing cross-device compatibility and significantly improving user experience across web platforms.',
+        'Seamlessly integrated Graphql APIs to facilitate secure and efficient data exchange between systems.',
+        'Employed Jasmine and Karma for automated unit testing, effectively identifying and resolving critical bugs, which resulted in a notable reduction in production issues and enhanced code quality.',
+        'Recommended and implemented Prime NG and NG-Toast libraries for popup notifications, and successfully demonstrated both solutions to clients.',
+        'Independently resolved all bugs from Team 3 and successfully delivered the fixes on time during project closure.',
+        'Engaged in ongoing collaboration with stakeholders to prioritize features and enhancements based on evolving business needs'
       ]
     },
     {
       name: 'Odyssey',
       techStack: 'Angular, JavaScript, JSP, Manual Testing',
       description: [
-        'Legacy application for US Automobiles Company.',
-        'Seamlessly integrated REST APIs.',
-        'Supported both Manual and Regression Testing.'
+        'Odyssey is a legacy Application which is built on Various technologies like Angular/JavaScript/Java 8 and used for to maintain Owner Manuals/Workshop Manuals of US Automobiles Company.',
+        'Seamlessly integrated REST APIs to facilitate secure data exchange between the UI and backend.',
+        'Worked on Enhancement team and also supported Testing team along with development in both Manual and regression Testing',
+        'Post production Hotfixes and cleared all the frontend bugs from enhancement team.',
+        'Provided Knowledge Transfer sessions to new joiners'
       ]
     },
     {
       name: 'Compass Modernization',
       techStack: 'Angular 17, CSS, HTML5, Bootstrap, NgRx, TypeScript',
       description: [
-        'Modernized legacy PowerBuilder application.',
-        'Created reusable UI components.',
-        'Integrated real-time backend services.',
-        'Implemented over 50 validations in a single screen.'
+        'Modernisation of legacy PowerBuilder Application to Angular and Springboot web application',
+        'Reusable Component Creation: Engineered a library of reusable UI components, streamlining the app development process and enhancing consistency across various projects.',
+        'Real-Time Backend Integration: Integrated real-time backend services, enabling dynamic data interaction between UI and Backend.',
+        'Developed new screens and functionalities according to the client requirement by cross verifying the validations with the legacy application',
+        'Implemented 50+ Validations in a single screen and successfully delivered to the customer',
+        'Initiated the implementation of NgRx in the application to manage state efficiently.'
       ]
     }
   ];
